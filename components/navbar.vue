92aff869c7352d5f4a0c61cbb8663d1c5b5fe0e3
Zulfikar <zulfikarromansyah065@gmail.com>
2023-05-25 13:56:23 +0700

    abc

---
+++ components/navbar.vue
@@ -0,0 +1,24 @@
+<template>
+<nav class="bg-transparent absolute flex justify-end">
+  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+    <img class="fill-current h-8 w-8 mr-4" width="54" height="54" viewBox="0 0 54 54" src="img/lg.png">
+    <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+      <span class="sr-only">Open main menu</span>
+      <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+    </button>
+    <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+      <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 md:flex-row md:space-x-8 md:mt-0 md:border-0">
+        <li class="group">
+                                    <nuxt-link to="/"
+                                        class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">Home</nuxt-link>
+                                </li>
+                                <li class="group">
+                                    <nuxt-link to="/about"
+                                        class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">About</nuxt-link>
+                                </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <main class="">
+      <section class="min-h-screen flex items-center bg-[url(img/bga.png)] md:bg-fixed">
+        <div class="max-w-sm rounded overflow-hidden shadow-lg ml-10 bg-white">
+  <div class="px-6 py-4">
+    <div class="font-bold text-xl mb-2">Rencanakan Perjalanan Anda Sekarang.</div>
+    <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+    <div class="mb-4">
+      <label class="block text-gray-700 text-sm font-bold mb-2">
+        Username
+      </label>
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username">
+    </div>
+    <div class="mb-4">
+      <label class="block text-gray-700 text-sm font-bold mb-2">
+        Berangkat
+      </label>
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="date" type="date">
+    </div>
+    <div class="mb-4">
+      <label class="block text-gray-700 text-sm font-bold mb-2">
+        Pulang
+      </label>
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="date" type="date">
+    </div>
+    <div class="flex items-center justify-end">
+      <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+        Sign In
+      </button>
+    </div>
+  </form>
+  </div>
+</div>
+      </section>
+      <section id="class">
+                <h2 class="font-bold text-4xl my-6">Exploler Airbnb</h2>
+                <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 gap-y-8">
+                    <div class="bg-white shadow-lg rounded" v-for="item in produkData" :key="item.id">
+                      <div class="flex flex-col rounded-lg bg-white  md:max-w-xl md:flex-row">
+  <img
+    class="h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+    :src="item.image"
+    alt="" />
+  <div class="flex flex-col justify-start p-6">
+    <h5 class="mb-2 text-xl font-medium">
+      {{ item.name }}
+    </h5>
+  </div>
+</div>
+                    </div>
+                </div>
+            </section>
+<p class="text-white">
+  Airbnb adalah jaringan pasar daring dan penginapan rumahan sejawat yang memungkinkan pengguna mendaftarkan atau menyewa properti untuk digunakan dalam jangka pendek. Harga sewanya ditetapkan oleh pemilik properti. Airbnb menerima sebagian tarif jasa pembukuan dari tamu dan tuan rumah.
+          </p>
+    </main>
+  </div>
+  </template>
+
+<script>
+import json from '@/assets/data/ex.json';
+export default {
+    data() {
+        return {
+            layouts: {
+                header: "header",
+                footer: "footer"
+            },
+            produkData: json,
+        }
+    },
+}
+</script>
